0af7d16003b3054846eaef04696b8591216856e9
Mauro Minella <mauro.minella@microsoft.com>
2025-10-15 09:08:34 +0200

    minor changes

--- server/A2A_03_CopilotStudioAgent/obj/Debug/net9.0/A2A_01_EchoAgent.AssemblyInfo.cs
+++ server/A2A_03_CopilotStudioAgent/obj/Debug/net9.0/A2A_01_EchoAgent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A2A_01_EchoAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfaaa09c296996d7cf88917ccf06bf4ac30ed79e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a8e687601f4d97fd9f2b51c90e36cab25e1f71")]
 [assembly: System.Reflection.AssemblyProductAttribute("A2A_01_EchoAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A2A_01_EchoAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
